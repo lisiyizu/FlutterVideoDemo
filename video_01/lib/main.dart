435296ce0b8a54870dfaf349643af41a539a95a8
xhmo <xhmo@mobvoi.com>
2019-04-17 23:18:23 +0800

    导航栏+导航拦按钮+导航栏下面的bottom

--- video_01/lib/main.dart
+++ video_01/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+// import 'model/post.dart';
+// import 'demo/ListView_demo.dart'; // 导入了没有使用的dart 也会报错
 
 void main() => runApp(App());
 
@@ -10,63 +11,58 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,  // 隐藏右上角debug标签
       home: Home(),
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.yellow, // 主题色
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5), // 高亮颜色
+        splashColor: Colors.white70,
       ),
     );
   }
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          SizedBox(height: 16.0),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: Text('莫小言'),
-        elevation: 0.0,
-      ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder:  _listItemBuilder,
-      ),
-    );
-  }
-}
-
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-        return Center(
-      child: Text(
-        'Hello',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.yellow,
-        )
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          // leading 导航栏 左边按钮
+          leading: IconButton(  
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigration', // 描述
+            onPressed: () => debugPrint('Navigation button is pressed'),   // null 不可点击状态
+          ),
+          title: Text('莫小言'),
+          // actions 导航栏 右边按钮数组
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: () => debugPrint('Navigation button is pressed'),
+            ),
+          ],
+          elevation: 0.0,
+          // DefaultTabController 才有
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38, // 未选中tabBtn的颜色
+            indicatorColor: Colors.black54,       // 下划线颜色
+            indicatorSize: TabBarIndicatorSize.label, // 下划线宽度
+            indicatorWeight: 1.0, // 下划线高度
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)), 
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ],
+        ),
       ),
     );
   }
