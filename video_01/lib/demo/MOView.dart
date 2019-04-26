ddd6454255fad87ce14da035d707019167a02673
xhmo <xhmo@mobvoi.com>
2019-04-26 23:22:22 +0800

    GridViewDemo

--- video_01/lib/demo/MOView.dart
+++ video_01/lib/demo/MOView.dart
@@ -2,7 +2,86 @@ import 'package:flutter/material.dart';
 import '../model/post.dart';
 
 class MOView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridViewBuilderDemo();
+  }
+}
 
+class GridViewBuilderDemo extends StatelessWidget {
+  Widget _gridItemBuilder(BuildContext context, int index) {
+    return Container(
+      child: Image.network(
+        posts[index].imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: posts.length,
+      itemBuilder: _gridItemBuilder,
+      padding: EdgeInsets.all(8.0),
+      // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //   crossAxisCount: 3,
+      //   crossAxisSpacing: 8.0,
+      //   mainAxisSpacing: 8.0,
+      // ),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 150.0,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+      ),
+    );
+  }
+}
+
+class GridViewExtentDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text('Item $index', style: TextStyle(fontSize: 18.0, color: Colors.grey)),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent( // 不固定count
+      maxCrossAxisExtent: 100.0, // request：item 在交叉轴上的长度
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text('Item $index', style: TextStyle(fontSize: 18.0, color: Colors.grey)),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 5,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
+    );
+  }
+}
+
+class PageViewBuilderDemo extends StatelessWidget {
   Widget _pageItemBuilder(BuildContext context, int index) {
     return Stack(
       children: <Widget>[
@@ -29,7 +108,6 @@ class MOView extends StatelessWidget {
       ],
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
